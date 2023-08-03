11c9faa3d7f9b2bb69d5fbafca8f40b120330473
XRhahelry <sujalbajracharya1@gmail.com>
2023-08-03 21:51:57 +0545

    ai done

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from pyray import *
 from box import *
-import random
-from minimax import *
 
 WIDTH, HEIGHT = 300, 300
 init_window(WIDTH, HEIGHT, "TIC TAC TOE")
@@ -16,6 +14,7 @@ board = [["", "", ""], ["", "", ""], ["", "", ""]]
 HUMAN = "O"
 AI = "X"
 turn = HUMAN
+num = 0
 
 
 def draw_screen():
@@ -25,7 +24,7 @@ def draw_screen():
     draw_line(0, 200, 300, 200, BLACK)
 
 
-def draw_board(board):
+def draw_board():
     count = 0
     for i in range(0,3):
         for j in range(0,3):
@@ -36,19 +35,11 @@ def draw_board(board):
             count += 1
 
 
-def turn_change(turn):
-    if turn == HUMAN:
-        return AI
-    else:
-        return HUMAN
-
-
 def equals3(a, b, c):
     return a == b and b == c and a != ""
 
 
-def check_win(board):
-    winner = 0
+def check_win():
     for i in range(0, 3):
         if equals3(board[0][i], board[1][i], board[2][i]):
             return board[0][i] 
@@ -63,11 +54,13 @@ def check_win(board):
     if equals3(board[2][0], board[1][1], board[0][2]):
         return board[1][1] 
 
-    if len(available_spots(board)) == 0:
+    if len(available_spots()) == 0:
         return "draw"
+    else:
+        return "wait"
 
 
-def available_spots(board):
+def available_spots():
     available = []
     for i in range(0,3):
         for j in range(0,3):
@@ -75,6 +68,53 @@ def available_spots(board):
                 available.append([i, j])
     return available
 
+def best_move():
+    best_score = -1000000
+    for i in range(0,3):
+        for j in range(0,3):
+            if board[i][j] == "":
+                board[i][j] = AI
+                score = minimax(HUMAN)
+                board[i][j] = ""
+                if score > best_score:
+                    best_score = score
+                    move = [i, j]
+    
+    return move
+
+scores = {
+    "X": 10,
+    "O": -10,
+    "draw": 0
+}
+
+def minimax(player):
+    result = check_win()
+    if result != "wait":
+        return scores[result]
+    
+    if player == AI:
+        best_score = -1000000
+        for i in range(0,3):
+            for j in range(0,3):
+                if board[i][j] == "":
+                    board[i][j] = AI
+                    score = minimax(HUMAN)
+                    board[i][j] = ""
+                    best_score = max(score, best_score)
+        
+        return best_score
+    else:
+        best_score = 1000000
+        for i in range(0,3):
+            for j in range(0,3):
+                if board[i][j] == "":
+                    board[i][j] = HUMAN
+                    score = minimax(AI)
+                    board[i][j] = ""
+                    best_score = min(score, best_score)
+        
+        return best_score
 
 while not window_should_close():
     begin_drawing()
@@ -82,63 +122,62 @@ while not window_should_close():
     draw_screen()
 
     if turn == AI:
-        available = available_spots(board)
-        x, y  = random.choice(available)
-        board[x][y] = AI
+        move = best_move()
+        count = 1
+        board[move[0]][move[1]] = AI
         turn = HUMAN
 
-    draw_board(board)
+    draw_board()
     if is_mouse_button_pressed(0):
         pos = get_mouse_position()
         if pos.x < 100:
             if pos.y < 100:
                 if board[0][0] == "":
                     board[0][0] = turn
-                    turn = "X"
+                    turn = AI
             elif pos.y < 200:
                 if board[1][0] == "":
                     board[1][0] = turn
-                    turn = "X"
+                    turn = AI
             else:
                 if board[2][0] == "":
                     board[2][0] = turn
-                    turn = "X"
+                    turn = AI
         elif pos.x < 200:
             if pos.y < 100:
                 if board[0][1] == "":
                     board[0][1] = turn
-                    turn = "X"
+                    turn = AI
             elif pos.y < 200:
                 if board[1][1] == "":
                     board[1][1] = turn
-                    turn = "X"
+                    turn = AI
             else:
                 if board[2][1] == "":
                     board[2][1] = turn
-                    turn = "X"
+                    turn = AI
         else:
             if pos.y < 100:
                 if board[0][2] == "":
                     board[0][2] = turn
-                    turn = "X"
+                    turn = AI
             elif pos.y < 200:
                 if board[1][2] == "":
                     board[1][2] = turn
-                    turn = "X"
+                    turn = AI
             else:
                 if board[2][2] == "":
                     board[2][2] = turn
-                    turn = "X"
+                    turn = AI
 
 
-    draw_board(board)
-    if check_win(board) == AI:
+    if check_win() == AI:
         draw_text("Player X won", 20, 120, 40, RED)
         turn = 0
-    elif check_win(board) == HUMAN:
+    elif check_win() == HUMAN:
         draw_text("Player O won", 20, 120, 40, RED)
         turn = 0
-    elif check_win(board) == "draw":
+    elif check_win() == "draw":
         draw_text("Draw", 60, 120, 40, RED) 
         turn = 0
 
